918166485f684cfd16ab5b696b96169623706544
tim <rosstimo@isu.edu>
2024-09-23 14:22:08 -0600

    fix rename bug

--- BetterForms/BetterForms/My Project/Application.Designer.vb
+++ BetterForms/BetterForms/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BetterForms.Form1
+            Me.MainForm = Global.BetterForms.PrimaryForm
         End Sub
     End Class
 End Namespace
