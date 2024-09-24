ecdd645d2570c2a0397a429e230922bf27c1a887
tim <rosstimo@isu.edu>
2024-09-24 12:41:06 -0600

    test array

---
+++ ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim fruits() As String
+        Dim temp(3) As String
+
+        temp(2) = "Orange"
+
+        fruits = temp
+
+        fruits = {"banana"}
+
+        Console.Read()
+
+    End Sub
+
+End Module
