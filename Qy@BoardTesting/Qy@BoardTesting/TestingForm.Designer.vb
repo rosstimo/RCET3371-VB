e14519de6f3585e00278e8dc85587a6e53ecf2af
tim <rosstimo@isu.edu>
2024-09-24 13:43:36 -0600

    add com test to be used to get settings

--- Qy@BoardTesting/Qy@BoardTesting/TestingForm.Designer.vb
+++ Qy@BoardTesting/Qy@BoardTesting/TestingForm.Designer.vb
@@ -23,19 +23,58 @@ Partial Class TestingForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'SerialPort
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(315, 332)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 62)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
         '
         'TestingForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "TestingForm"
         Me.Text = "Form1"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
 End Class
--- Qy@BoardTesting/Qy@BoardTesting/TestingForm.vb
+++ Qy@BoardTesting/Qy@BoardTesting/TestingForm.vb
@@ -1,3 +1,40 @@
 ﻿Public Class TestingForm
+    Private Sub TestingForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SerialPort.PortName = "COM5"
+        SerialPort.BaudRate = 9600
+        SerialPort.Open()
+    End Sub
+
+    Sub TestCom()
+        'create an array of bytes called data
+        Dim data(15) As Byte ' 16 bytes
+        data(0) = &B11110000
+
+        'send the byte to the serial port
+        SerialPort.Write(data, 0, 1) 'send 1 byte from data starting at 0
+
+
+    End Sub
+
+    Private Sub SerialPort_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        'read the data from the serial port
+        Dim receivedBytes(64) As Byte
+        SerialPort.Read(receivedBytes, 0, 64)
+
+        For i = 0 To UBound(receivedBytes)
+            'print the ascii character of the byte if it is a printable character otherwise print a dot
+            Console.Write($" {If(receivedBytes(i) >= 32 And receivedBytes(i) <= 126, Chr(receivedBytes(i)), ".")}")
+            'print all the bytes in binary
+            Console.Write($" &B{Convert.ToString(receivedBytes(i), 2).PadLeft(8, Chr(48))}")
+            'print all the bytes in hex
+            Console.WriteLine($"Byte {i} is: 0x{Hex(receivedBytes(i))}")
+        Next
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TestCom()
+    End Sub
 
 End Class
