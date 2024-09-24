c0b74fe7c31fb27ee86c57bdd84cf8ad851435cc
tim <rosstimo@isu.edu>
2024-09-24 09:17:37 -0600

    fix rename bug

--- MoreFormControls/MoreFormControls/My Project/Application.Designer.vb
+++ MoreFormControls/MoreFormControls/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MoreFormControls.Form1
+            Me.MainForm = Global.MoreFormControls.MoreControlsForm
         End Sub
     End Class
 End Namespace
