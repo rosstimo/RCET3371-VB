2e04cc6514c27f4fecbe5941a73809948acb78b7
tim <rosstimo@isu.edu>
2024-09-26 09:48:24 -0600

    working example

--- SerialComExample/SerialComExample/SerialComExampleForm.Designer.vb
+++ SerialComExample/SerialComExample/SerialComExampleForm.Designer.vb
@@ -22,10 +22,36 @@ Partial Class SerialComExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SerialPort
+        '
+        Me.SerialPort.Parity = System.IO.Ports.Parity.Odd
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(273, 104)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(151, 86)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'SerialComExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "SerialComExampleForm"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents Button1 As Button
 End Class
--- SerialComExample/SerialComExample/SerialComExampleForm.vb
+++ SerialComExample/SerialComExample/SerialComExampleForm.vb
@@ -1,3 +1,24 @@
 ﻿Public Class SerialComExampleForm
+    Private Sub SerialComExampleForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SerialPort.PortName = "COM4"
+        SerialPort.BaudRate = 9600
+        SerialPort.Open()
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim data(0) As Byte
+        data(0) = &B11110000
+        SerialPort.Write(data, 0, 1)
+    End Sub
+
+    Private Sub SerialPort_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        Dim data(SerialPort.BytesToRead) As Byte
+        SerialPort.Read(data, 0, SerialPort.BytesToRead)
+        For i = 0 To UBound(data)
+            Console.Write($"{Hex(data(i))}: ")
+            Console.WriteLine(Chr(data(i)))
+
+        Next
+    End Sub
 End Class
