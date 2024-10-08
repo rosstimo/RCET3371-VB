dbf46a06130001a33c0e530ad19d857b587b0816
tim <rosstimo@isu.edu>
2024-10-08 10:56:13 -0600

    add plot

--- ShiftingArrays/ShiftingArrays/ShiftingArraysForm.Designer.vb
+++ ShiftingArrays/ShiftingArrays/ShiftingArraysForm.Designer.vb
@@ -22,10 +22,71 @@ Partial Class ShiftingArraysForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.PlotPictureBox = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PlotButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        CType(Me.PlotPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PlotPictureBox
+        '
+        Me.PlotPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PlotPictureBox.Location = New System.Drawing.Point(12, 38)
+        Me.PlotPictureBox.Name = "PlotPictureBox"
+        Me.PlotPictureBox.Size = New System.Drawing.Size(776, 312)
+        Me.PlotPictureBox.TabIndex = 0
+        Me.PlotPictureBox.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.ExitButton)
+        Me.GroupBox1.Controls.Add(Me.PlotButton)
+        Me.GroupBox1.Location = New System.Drawing.Point(508, 356)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(280, 100)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'PlotButton
+        '
+        Me.PlotButton.Location = New System.Drawing.Point(31, 25)
+        Me.PlotButton.Name = "PlotButton"
+        Me.PlotButton.Size = New System.Drawing.Size(108, 57)
+        Me.PlotButton.TabIndex = 0
+        Me.PlotButton.Text = "&Plot"
+        Me.PlotButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(145, 25)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(108, 57)
+        Me.ExitButton.TabIndex = 1
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ShiftingArraysForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(803, 471)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PlotPictureBox)
+        Me.Name = "ShiftingArraysForm"
         Me.Text = "Form1"
+        CType(Me.PlotPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents PlotPictureBox As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents PlotButton As Button
 End Class
--- ShiftingArrays/ShiftingArrays/ShiftingArraysForm.vb
+++ ShiftingArrays/ShiftingArrays/ShiftingArraysForm.vb
@@ -1,3 +1,72 @@
 ﻿Public Class ShiftingArraysForm
+    Private Sub ShiftingArraysForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        Dim temp() As Integer
+        temp = ShiftArray(RandomNumberFrom(50, 100))
+        PlotPictureBox.Refresh()
+        Plot(temp)
+
+        ' Console.Read()
+    End Sub
+
+    Function ShiftArray(newData As Integer) As Integer()
+        Static data(99) As Integer
+
+        For i = LBound(data) To UBound(data) - 1
+            data(i) = data(i + 1)
+        Next
+
+        data(UBound(data)) = newData
+
+        Return data
+    End Function
+
+    Sub Plot(plotData() As Integer)
+        Dim g As Graphics = PlotPictureBox.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+        Dim oldX%, oldY%
+        g.ScaleTransform(CSng(PlotPictureBox.Width / 100), 1)
+        For x = 0 To UBound(plotData)
+            g.DrawLine(pen, oldX, oldY, x, plotData(x))
+            oldX = x
+            oldY = plotData(x)
+        Next
+
+    End Sub
+
+    Sub TestRandomness()
+        Dim randomness(10) As Integer
+
+        For i = 1 To 100000
+            randomness(RandomNumberFrom(0, 10)) += 1
+        Next
+
+        'Console.Clear()
+
+        For i = LBound(randomness) To UBound(randomness)
+            Console.Write(CStr(randomness(i)).PadLeft(6) & " |")
+        Next
+
+        Console.WriteLine()
+
+    End Sub
+
+    ''' <summary>
+    ''' returns an integer from min to max inclusive.
+    ''' <br/>
+    ''' defaults:
+    ''' <br/>
+    ''' min = 0
+    ''' <br/>
+    ''' max = 10
+    ''' </summary>
+    ''' <param name="min%"></param>
+    ''' <param name="max%"></param>
+    ''' <returns></returns>
+    Function RandomNumberFrom(Optional min% = 0, Optional max% = 10) As Integer
+        Dim _random%
+        Randomize(DateTime.Now.Millisecond)
+        _random = CInt(Math.Floor(Rnd() * (max + 1 - min))) + min
+        Return _random
+    End Function
 
 End Class
