35d879fc3deab93e82465cd079d85a9184681932
tim <rosstimo@isu.edu>
2024-10-17 09:26:29 -0600

    move event handlers

--- SerialComExample/SerialComExample/SerialComExampleForm.vb
+++ SerialComExample/SerialComExample/SerialComExampleForm.vb
@@ -1,23 +1,5 @@
 ﻿Imports System.Threading.Thread
 Public Class SerialComExampleForm
-    Private Sub SerialComExampleForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        SerialPort.PortName = "COM4"
-        SerialPort.BaudRate = 9600
-        SerialPort.Open()
-
-    End Sub
-
-
-
-    Private Sub SerialPort_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) 'Handles SerialPort.DataReceived
-        Dim data(SerialPort.BytesToRead) As Byte
-        SerialPort.Read(data, 0, SerialPort.BytesToRead)
-        For i = 0 To UBound(data)
-            Console.Write($"{Hex(data(i))}: ")
-            Console.WriteLine(Chr(data(i)))
-
-        Next
-    End Sub
     Sub WriteDigital()
         Dim Data(1) As Byte
         Data(0) = &H20 '&B101111
@@ -56,6 +38,24 @@ Public Class SerialComExampleForm
     End Function
 
     ' Event Handlers ----------------------------------------------------------------------------
+    Private Sub SerialComExampleForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SerialPort.PortName = "COM4"
+        SerialPort.BaudRate = 9600
+        SerialPort.Open()
+
+    End Sub
+
+
+
+    Private Sub SerialPort_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) 'Handles SerialPort.DataReceived
+        Dim data(SerialPort.BytesToRead) As Byte
+        SerialPort.Read(data, 0, SerialPort.BytesToRead)
+        For i = 0 To UBound(data)
+            Console.Write($"{Hex(data(i))}: ")
+            Console.WriteLine(Chr(data(i)))
+
+        Next
+    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'WriteDigital()
 
