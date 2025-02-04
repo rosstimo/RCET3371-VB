dd550124ef26387d95048c7a65a56088d8be695e
tim <rosstimo@isu.edu>
2025-02-04 09:28:35 -0700

    initial control setup

--- GraphicsExamples/GraphicsExamples/GraphicsForm.Designer.vb
+++ GraphicsExamples/GraphicsExamples/GraphicsForm.Designer.vb
@@ -22,10 +22,76 @@ Partial Class GraphicsForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DrawPictureBox = New System.Windows.Forms.PictureBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DrawButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        CType(Me.DrawPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DrawPictureBox
+        '
+        Me.DrawPictureBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DrawPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.DrawPictureBox.Location = New System.Drawing.Point(31, 24)
+        Me.DrawPictureBox.Name = "DrawPictureBox"
+        Me.DrawPictureBox.Size = New System.Drawing.Size(719, 412)
+        Me.DrawPictureBox.TabIndex = 0
+        Me.DrawPictureBox.TabStop = False
+        Me.DrawPictureBox.UseWaitCursor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ExitButton.Location = New System.Drawing.Point(613, 452)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(137, 75)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DrawButton
+        '
+        Me.DrawButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DrawButton.Location = New System.Drawing.Point(327, 452)
+        Me.DrawButton.Name = "DrawButton"
+        Me.DrawButton.Size = New System.Drawing.Size(137, 75)
+        Me.DrawButton.TabIndex = 0
+        Me.DrawButton.Text = "D&raw"
+        Me.DrawButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ClearButton.Location = New System.Drawing.Point(470, 452)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(137, 75)
+        Me.ClearButton.TabIndex = 1
+        Me.ClearButton.Text = "C&lear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'GraphicsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(778, 544)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DrawButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.DrawPictureBox)
+        Me.MinimumSize = New System.Drawing.Size(500, 500)
+        Me.Name = "GraphicsForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Graphics!"
+        CType(Me.DrawPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DrawPictureBox As PictureBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DrawButton As Button
+    Friend WithEvents ClearButton As Button
 End Class
