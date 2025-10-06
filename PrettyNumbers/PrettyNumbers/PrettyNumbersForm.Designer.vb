ca55d1620f750eaca338c97fe91b7d949b4bb7f7
rosstimo <rosstimo@isu.edu>
2025-10-06 10:14:06 -0600

    add engineering notation with metric prefixes

--- PrettyNumbers/PrettyNumbers/PrettyNumbersForm.Designer.vb
+++ PrettyNumbers/PrettyNumbers/PrettyNumbersForm.Designer.vb
@@ -27,67 +27,98 @@ Partial Class PrettyNumbersForm
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.CalculateButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.DisplayStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
+        Me.StatusStrip.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.InputTextBox)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 14)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(464, 224)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(567, 269)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         '
         'InputTextBox
         '
-        Me.InputTextBox.Location = New System.Drawing.Point(6, 25)
+        Me.InputTextBox.Location = New System.Drawing.Point(7, 30)
+        Me.InputTextBox.Margin = New System.Windows.Forms.Padding(4)
         Me.InputTextBox.Name = "InputTextBox"
-        Me.InputTextBox.Size = New System.Drawing.Size(311, 26)
+        Me.InputTextBox.Size = New System.Drawing.Size(379, 29)
         Me.InputTextBox.TabIndex = 0
         '
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.CalculateButton)
         Me.GroupBox2.Controls.Add(Me.ExitButton)
-        Me.GroupBox2.Location = New System.Drawing.Point(134, 242)
+        Me.GroupBox2.Location = New System.Drawing.Point(164, 290)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(342, 133)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox2.Size = New System.Drawing.Size(418, 160)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         '
         'CalculateButton
         '
-        Me.CalculateButton.Location = New System.Drawing.Point(6, 25)
+        Me.CalculateButton.Location = New System.Drawing.Point(7, 30)
+        Me.CalculateButton.Margin = New System.Windows.Forms.Padding(4)
         Me.CalculateButton.Name = "CalculateButton"
-        Me.CalculateButton.Size = New System.Drawing.Size(157, 92)
+        Me.CalculateButton.Size = New System.Drawing.Size(192, 110)
         Me.CalculateButton.TabIndex = 0
         Me.CalculateButton.Text = "&Calculate"
         Me.CalculateButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(169, 25)
+        Me.ExitButton.Location = New System.Drawing.Point(207, 30)
+        Me.ExitButton.Margin = New System.Windows.Forms.Padding(4)
         Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(157, 92)
+        Me.ExitButton.Size = New System.Drawing.Size(192, 110)
         Me.ExitButton.TabIndex = 1
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
+        'StatusStrip
+        '
+        Me.StatusStrip.ImageScalingSize = New System.Drawing.Size(28, 28)
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DisplayStatusLabel})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 451)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(599, 39)
+        Me.StatusStrip.TabIndex = 2
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'DisplayStatusLabel
+        '
+        Me.DisplayStatusLabel.Name = "DisplayStatusLabel"
+        Me.DisplayStatusLabel.Size = New System.Drawing.Size(62, 30)
+        Me.DisplayStatusLabel.Text = "value"
+        '
         'PrettyNumbersForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(490, 408)
+        Me.ClientSize = New System.Drawing.Size(599, 490)
+        Me.Controls.Add(Me.StatusStrip)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "PrettyNumbersForm"
         Me.Text = "Pretty Numbers"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -96,4 +127,6 @@ Partial Class PrettyNumbersForm
     Friend WithEvents CalculateButton As Button
     Friend WithEvents ExitButton As Button
     Friend WithEvents InputTextBox As TextBox
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents DisplayStatusLabel As ToolStripStatusLabel
 End Class
--- PrettyNumbers/PrettyNumbers/PrettyNumbersForm.vb
+++ PrettyNumbers/PrettyNumbers/PrettyNumbersForm.vb
@@ -1,4 +1,66 @@
-﻿Public Class PrettyNumbersForm
+﻿Imports System.CodeDom.Compiler
+
+Public Class PrettyNumbersForm
+
+
+    Private Function EngineeringNotation(value As Decimal, fix As Integer) As String
+        Dim valueString$
+        Dim valueSplit() As String
+        Dim mantissa@, exponent%
+
+        'valueString = value.ToString("0.0000000000000E+00")
+        valueString = value.ToString("E")
+        valueSplit = Split(valueString, "E")
+
+        mantissa = CDec(valueSplit(0))
+        exponent = CInt(valueSplit(1))
+
+        Do Until exponent Mod 3 = 0
+            mantissa *= 10
+            exponent -= 1
+        Loop
+
+        'valueString.PadLeft(5)
+
+        Return $"{mantissa.ToString.Remove(fix + 1)}E{exponent}"
+    End Function
+
+    ''' <summary>
+    ''' Convert Engineering Notation into pretty numbers with metric prefix
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function Pretty(engineeringNotation As String) As String
+        Dim temp() As String
+
+        temp = Split(engineeringNotation, "E")
+        Select Case temp(1)
+            Case "9"
+                temp(1) = "G"
+            Case "6"
+                temp(1) = "M"
+            Case "3"
+                temp(1) = "k"
+            Case "0"
+                temp(1) = ""
+            Case "-3"
+                temp(1) = "m"
+            Case "-6"
+                temp(1) = ChrW(&HB5)
+            Case "-9"
+                temp(1) = "n"
+            Case "-12"
+                temp(1) = "p"
+            Case Else
+                'pass
+                MsgBox("BUG: Edge Case")
+        End Select
+        Return $"{temp(0)}{temp(1)}"
+    End Function
+
+    Sub Display(displayValue As String)
+        DisplayStatusLabel.Text = displayValue
+    End Sub
+    'Event Handlers -----------------------------------------------------------
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
@@ -6,14 +68,12 @@
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
         If InputTextBox.Text <> "" And IsNumeric(InputTextBox.Text) Then
             'get text from textbox1 and format in scientific notation using ToString method
-            InputTextBox.Text = CDec(InputTextBox.Text).ToString("0.00E+00")
-
-
-
-
-
+            'Display(CDec(InputTextBox.Text).ToString("0.0000E+00"))
+            Display(Pretty(EngineeringNotation(InputTextBox.Text, 6)))
 
 
         End If
     End Sub
+
+
 End Class
