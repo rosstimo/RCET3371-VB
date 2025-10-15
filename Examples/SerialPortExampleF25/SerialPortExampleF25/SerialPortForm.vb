49a9bedc5b7eb841e9d92c0a71ec7497bffc5ae5
rosstimo <rosstimo@isu.edu>
2025-10-15 11:02:13 -0600

    add write, read, and check for q@ Board Examples

--- Examples/SerialPortExampleF25/SerialPortExampleF25/SerialPortForm.vb
+++ Examples/SerialPortExampleF25/SerialPortExampleF25/SerialPortForm.vb
@@ -12,18 +12,61 @@ Public Class SerialPortForm
         SerialPort1.PortName = "COM4"
 
         SerialPort1.Open()
-        'SerialPort1.Open()
-        'SerialPort1.Close()
 
     End Sub
 
+    Sub Write()
+        Dim data(0) As Byte 'put bytes into array
+        data(0) = &B11110000 'actual data as a byte
+        SerialPort1.Write(data, 0, 1) 'send bytes as array, start at index 0, send 1 byte
+    End Sub
 
+    Sub Read()
+        Dim data(SerialPort1.BytesToRead) As Byte
+
+        SerialPort1.Read(data, 0, SerialPort1.BytesToRead)
+
+        For i = 0 To UBound(data)
+            Console.WriteLine($"Byte {i}: {Chr(data(i))}")
+        Next
+
+        Console.WriteLine($"Is this Q@ Board: {IsQuietBoard(data)}")
+        Console.WriteLine(UBound(data))
+
+    End Sub
+
+    Function IsQuietBoard(data() As Byte) As Boolean
+
+
+        If UBound(data) = 64 And Chr(data(60)) = "@" Then
+            Return True
+        Else
+            Return False
+        End If
+
+
+    End Function
+
+    Function CheckIfQuietBoard() As Boolean
+        Dim bytes(0) As Byte
+        bytes(0) = &B11110000
+        SerialPort1.Write(bytes, 0, 1)
+        Return True
+    End Function
 
     Private Sub SerialPortForm_Click(sender As Object, e As EventArgs) Handles Me.Click
         Connect()
+        Write()
     End Sub
 
     Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
-        Me.Text = SerialPort1.BytesToRead.ToString
+        Try
+            Console.WriteLine(SerialPort1.BytesToRead)
+        Catch ex As Exception
+            Console.WriteLine("oops!")
+        End Try
+
+        Read()
+
     End Sub
 End Class
