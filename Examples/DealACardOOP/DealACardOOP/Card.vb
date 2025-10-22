e7a7d2831b3ed4b981991c876b51a2b8f2e712f4
rosstimo <rosstimo@isu.edu>
2025-10-22 10:33:36 -0600

    add working card class

---
+++ Examples/DealACardOOP/DealACardOOP/Card.vb
@@ -0,0 +1,62 @@
+﻿Public Class Card
+
+    Private _suit As String
+    Public Property suit() As String
+        Get
+            Return _suit
+        End Get
+        Set(ByVal value As String)
+            _suit = value
+        End Set
+    End Property
+
+    Private _rank As String
+    Public Property rank() As String
+        Get
+            Return _rank
+        End Get
+        Set(ByVal value As String)
+            _rank = value
+        End Set
+    End Property
+
+    Private _frontImage As Image
+    Public Property frontImage() As Image
+        Get
+            Return _frontImage
+        End Get
+        Set(ByVal value As Image)
+            _frontImage = value
+        End Set
+    End Property
+
+    Private _backImage As Image
+    Public Property backImage() As Image
+        Get
+            Return _backImage
+        End Get
+        Set(ByVal value As Image)
+            _backImage = value
+        End Set
+    End Property
+
+    Sub New(rank As String, suit As String)
+
+        Me.suit = suit
+        Me.rank = rank
+
+
+        Me.frontImage = My.Resources.ResourceManager.GetObject($"{Me.rank}{Me.suit}")
+        If Me.frontImage Is Nothing Then
+            Me.frontImage = My.Resources.ResourceManager.GetObject($"_{Me.rank}{Me.suit}")
+        End If
+        If Me.frontImage Is Nothing Then
+            Me.frontImage = My.Resources.aces
+        End If
+
+        Me.backImage = My.Resources.CardBack
+
+    End Sub
+
+
+End Class
--- Examples/DealACardOOP/DealACardOOP/MainForm.vb
+++ Examples/DealACardOOP/DealACardOOP/MainForm.vb
@@ -1,3 +1,16 @@
 ﻿Public Class MainForm
+    Private Sub MainForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        Dim ah As New Card("W", "H")
+        'Dim cardName As String = "_AS"
+        'Dim cardImage As Image = My.Resources.ResourceManager.GetObject(cardName) ' My.Resources._AS
+        Dim g As Graphics = Me.CreateGraphics
+
+
+
+        g.DrawImage(ah.backImage, 10, 10)
+        g.DrawImage(ah.frontImage, 100, 100)
+
+        g.Dispose()
+    End Sub
 
 End Class
