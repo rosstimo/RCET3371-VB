77fd97c0d22de13ff415b0684af2ae2de1b937ea
rosstimo <rosstimo@isu.edu>
2025-10-22 09:19:08 -0600

    init

---
+++ Examples/DealACardOOP/DealACardOOP/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DealACardOOP")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University College of Technology")>
+<Assembly: AssemblyProduct("DealACardOOP")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University College of Technology 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9c63c913-b3a2-4c4d-8546-5aa312aa03f9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
